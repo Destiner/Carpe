77cb36c09aedcb7acff02fb6574a10b11db447ee
Timur Badretdinov <accounts@destiner.io>
2025-06-27 20:12:26 +0100

    Initial Commit

---
+++ CarpeTests/CarpeTests.swift
@@ -0,0 +1,16 @@
+//
+//  CarpeTests.swift
+//  CarpeTests
+//
+//  Created by Timur Badretdinov on 27/06/2025.
+//
+
+import Testing
+
+struct CarpeTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
